ecb936defb009a9b8fb1699a5930d22705ca5623
Nurlibay Koshkinbaev <koshkinbayevn@gmail.com>
2022-10-04 00:47:11 +0500

    LocalBroadcastManager added

--- broadcast2/src/main/java/uz/unidev/broadcast2/MainActivity.kt
+++ broadcast2/src/main/java/uz/unidev/broadcast2/MainActivity.kt
@@ -1,20 +1,14 @@
-package uz.unidev.broadcastreceiver
+package uz.unidev.broadcast2
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
-
-    private val localBroadcastManager by lazy {
-        LocalBroadcastManager.getInstance(this)
-    }
 
     private lateinit var progressBar: ProgressBar
 
@@ -25,34 +19,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         progressBar = findViewById(R.id.progressBar)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            Intent(MyReceiver.ACTION_CLICKED).apply {
-                putExtra(MyReceiver.EXTRA_COUNT, count++)
-                localBroadcastManager.sendBroadcast(this)
-            }
-        }
 
         IntentFilter().apply {
-            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            addAction(Intent.ACTION_BATTERY_LOW)
-            addAction(MyReceiver.ACTION_CLICKED)
             addAction("loaded")
-            localBroadcastManager.registerReceiver(receiver, this)
-        }
-
-        Intent(this, MyService::class.java).apply {
-            startService(this)
+            registerReceiver(receiver, this)
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        localBroadcastManager.unregisterReceiver(receiver)
+        unregisterReceiver(receiver)
     }
 }

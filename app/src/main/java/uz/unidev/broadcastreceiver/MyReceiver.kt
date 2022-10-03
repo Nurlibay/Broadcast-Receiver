3505fac3951b370490a6fee2aeb3bbf2f9f5e951
Nurlibay Koshkinbaev <koshkinbayevn@gmail.com>
2022-10-04 00:04:11 +0500

    custom action was added

--- app/src/main/java/uz/unidev/broadcastreceiver/MyReceiver.kt
+++ app/src/main/java/uz/unidev/broadcastreceiver/MyReceiver.kt
@@ -13,6 +13,10 @@ class MyReceiver: BroadcastReceiver()  {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         when(intent?.action){
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked: $count", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 val turnedOn = intent.getBooleanExtra("state", false)
                 Toast.makeText(context, "Airplane mode changed. Turned on: $turnedOn", Toast.LENGTH_SHORT).show()
@@ -21,5 +25,10 @@ class MyReceiver: BroadcastReceiver()  {
                 Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
